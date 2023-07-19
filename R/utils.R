67ec6ca08b88095be5e56636206243f3d00e9c4f
Chris Pyles <cpyles@berkeley.edu>
2023-07-18 17:56:36 -0700

    remove make_secret

--- R/utils.R
+++ R/utils.R
@@ -1,30 +1,3 @@
-#' A string containing characters that can be made into a valid variable name. Does not include any
-#' digits because randomly sampling with them included could result in an invalid variable name.
-valid_expr_chars <- "abcdefghijklmnopqrstuvwxyzABCDEFGHIJLKMNOPQRSTUVWXYZ._"
-
-#' Generate a random string of characters
-#'
-#' @description Randomly generate a string of `n_chars` sampled at random from `valid_chars`.
-#'
-#' @param n_chars The number of characters in the string; defaults to 6
-#' @param valid_chars A string of characters to choose from; defaults to all alphanumerals, `.`, and
-#' `_`
-#'
-#' @return The generated string
-make_secret <- function(n_chars, valid_chars) {
-  if (missing(n_chars)) {
-    n_chars <- 6
-  }
-
-  if (missing(valid_chars)) {
-    valid_chars <- strsplit(valid_expr_chars, "")[[1]]
-  }
-
-  chars <- sample(valid_chars, n_chars, replace = TRUE)
-  return(paste(chars, collapse = ""))
-}
-
-
 #' Check whether a string is valid R code
 #'
 #' @description Determine whether a code snippet has any syntax errors.
--- tests/testthat/test_make_secret.R
+++
@@ -1,6 +0,0 @@
-test_that("generates a random string of characters", {
-  set.seed(42)
-  expect_equal(make_secret(), "WLayjJ")
-  expect_equal(make_secret(10), "rWUxgJyLTt")
-  expect_equal(make_secret(20, c("a", "b", "c", "1", "2", "3")), "bbcaac12221b1cbab3c3")
-})
