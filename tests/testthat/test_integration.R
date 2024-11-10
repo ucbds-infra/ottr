8ceec173bd3e3d1ca26b014e6d714a53420be643
Chris Pyles <chris@chrispyles.io>
2024-11-09 17:11:39 -0800

    fix R-devel errors from https://cran.r-project.org/web/checks/check_results_ottr.html

--- tests/testthat/test_integration.R
+++ tests/testthat/test_integration.R
@@ -10,13 +10,25 @@ test_that("components integrate correctly", {
     collapse = "\n"
   )
 
-  run_test <- function(code, expected_json_path) {
+  run_test <- function(code, ...) {
     code_with_checks <- paste0(code, "\n", check_code)
     writeLines(code_with_checks, script_path, sep = "\n")
     results_json <- run_autograder(script_path)
-    expect_equal(
-      as.character(stringr::str_replace_all(results_json, r"(\\u001b\[\d\dm)", "")),
-      paste(readLines(expected_json_path, warn = FALSE), collapse = "\n"))
+    got <- as.character(stringr::str_replace_all(results_json, r"(\\u001b\[\d\dm)", ""))
+    want <- NULL
+    # go through all valid JSON file paths and check if any matches; if it does, use that one, otherwise
+    # use the first one
+    for (p in c(...)) {
+      pc <- paste(readLines(p, warn = FALSE), collapse = "\n")
+      if (is.null(want)) {
+        want <- pc
+      }
+      if (got == pc) {
+        want <- pc
+        break
+      }
+    }
+    testthat::expect_equal(want, got)
   }
 
   passes_all <- "
@@ -24,11 +36,20 @@ test_that("components integrate correctly", {
     square <- function(x) x ^ 2
     y <- paste0(\"hi there, \", c(\"a\", \"b\", \"c\"), \"!\")
   "
-  run_test(passes_all, test_path("files", "integration_expected_results", "passes_all.json"))
+  run_test(
+    passes_all,
+    test_path("files", "integration_expected_results", "passes_all.json"),
+    test_path("files", "integration_expected_results_rounding", "passes_all.json"))
 
   fails_2_hidden <- paste0(passes_all, "\nsquare <- function(x) 1")
-  run_test(fails_2_hidden, test_path("files", "integration_expected_results", "fails_2_hidden.json"))
+  run_test(
+    fails_2_hidden,
+    test_path("files", "integration_expected_results", "fails_2_hidden.json"),
+    test_path("files", "integration_expected_results_rounding", "fails_2_hidden.json"))
 
   fails_3 <- paste0(passes_all, "\ny <- c(1, 2)")
-  run_test(fails_3, test_path("files", "integration_expected_results", "fails_3.json"))
+  run_test(
+    fails_3,
+    test_path("files", "integration_expected_results", "fails_3.json"),
+    test_path("files", "integration_expected_results_rounding", "fails_3.json"))
 })
